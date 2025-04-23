ac65360848432fe65d9ed6a5855a3ca5885b82b5
Tcxroy <51190540+Tcxroy@users.noreply.github.com>
2025-04-23 15:24:14 +0200

    Update data_path.py

--- src/helpers/data_path.py
+++ src/helpers/data_path.py
@@ -4,8 +4,7 @@ def get_data_path(file_name):
     try:
         cur_dir = os.path.abspath(os.path.curdir)
         data_dir_1 = 'src'
-        data_dir_2 = 'data'
-        data_path = os.path.join(cur_dir,data_dir_1,data_dir_2,file_name)
+        data_path = os.path.join(cur_dir,data_dir_1,file_name)
     except:
         raise("file name should be a string")
     return data_path
